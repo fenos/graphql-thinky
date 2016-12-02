414544be107980aa0e94a35ce2ec9d0bc0ce2c02
Fabrizio <fabri_feno@yahoo.it>
2016-12-02 23:01:02 +0000

    default db port on testing

--- test/helpers/db/thinky.js
+++ test/helpers/db/thinky.js
@@ -1,7 +1,5 @@
 import thinky from 'thinky';
 
 export default thinky({
-  db: "think_graph_test_" + process.pid,
-  host: '192.168.99.100',
-  port: 32769
+  db: "think_graph_test_" + process.pid
 });
